4b096f94fe2dfa67e0a0d1d71173da677df37d90
Etherpod <etherpod@gmail.com>
2025-02-08 02:48:17 -0800

    Add a bunch more Ernesto dialogue

--- ShipEnhancements/CockpitErnesto.cs
+++ ShipEnhancements/CockpitErnesto.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace ShipEnhancements;
 
@@ -8,13 +9,18 @@ public class CockpitErnesto : MonoBehaviour
     private InteractReceiver _conversationZone = null;
 
     private CharacterDialogueTree _dialogueTree;
+    private List<int> _questions = [];
+    private List<int> _availableQuestions = [];
 
     private void Awake()
     {
         _dialogueTree = _conversationZone.GetComponent<CharacterDialogueTree>();
 
+        _dialogueTree.OnStartConversation += OnStartConversation;
         GlobalMessenger<OWRigidbody>.AddListener("EnterFlightConsole", OnEnterFlightConsole);
         GlobalMessenger.AddListener("ExitFlightConsole", OnExitFlightConsole);
+
+        ResetAvailableQuestions();
     }
 
     private void Start()
@@ -22,6 +28,45 @@ public class CockpitErnesto : MonoBehaviour
         if (ErnestoModListHandler.ActiveModList.Count > 0)
         {
             DialogueConditionManager.SharedInstance.SetConditionState("SE_MULTIPLE_ERNESTOS", true);
+        }
+    }
+
+    private void ResetAvailableQuestions()
+    {
+        _questions.Clear();
+        _availableQuestions.Clear();
+
+        for (int i = 1; i < 23; i++)
+        {
+            _questions.Add(i);
+        }
+
+        if (DialogueConditionManager.SharedInstance.GetConditionState("SE_ERNESTO_GESWALDO_PART_ONE"))
+        {
+            _questions.Add(101);
+        }
+        else
+        {
+            _questions.Add(100);
+        }
+
+        _availableQuestions.AddRange(_questions);
+    }
+
+    private void OnStartConversation()
+    {
+        foreach (int i in _questions)
+        {
+            DialogueConditionManager.SharedInstance.SetConditionState("SE_ERNESTO_OPTION_" + i, false);
+        }
+
+        int num = _availableQuestions[Random.Range(0, _availableQuestions.Count)];
+        DialogueConditionManager.SharedInstance.SetConditionState("SE_ERNESTO_OPTION_" + num, true);
+        _availableQuestions.Remove(num);
+
+        if (_availableQuestions.Count == 0)
+        {
+            ResetAvailableQuestions();
         }
     }
 
@@ -37,6 +82,7 @@ public class CockpitErnesto : MonoBehaviour
 
     private void OnDestroy()
     {
+        _dialogueTree.OnStartConversation -= OnStartConversation;
         GlobalMessenger<OWRigidbody>.RemoveListener("EnterFlightConsole", OnEnterFlightConsole);
         GlobalMessenger.RemoveListener("ExitFlightConsole", OnExitFlightConsole);
     }
