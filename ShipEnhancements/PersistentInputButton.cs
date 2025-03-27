ac67a3ec23fdd1b6aafc65e9a49a94438a1aa998
Etherpod <etherpod@gmail.com>
2025-03-26 21:30:44 -0700

    Start on persistent input button

---
+++ ShipEnhancements/PersistentInputButton.cs
@@ -0,0 +1,27 @@
+﻿namespace ShipEnhancements;
+
+public class PersistentInputButton : CockpitButton
+{
+    private ShipPersistentInput _persistentInput;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        ShipEnhancements.WriteDebugMessage("Persistent input start");
+        _persistentInput = SELocator.GetShipBody().GetComponent<ShipPersistentInput>();
+        _persistentInput.SetInputEnabled(_on && !SELocator.GetShipDamageController().IsElectricalFailed());
+    }
+
+    protected override void OnChangeState()
+    {
+        _persistentInput.SetInputEnabled(_on);
+    }
+
+    public override void SetPowered(bool powered)
+    {
+        base.SetPowered(powered);
+        if (_electricalDisrupted) return;
+        _persistentInput.SetInputEnabled(_on && !SELocator.GetShipDamageController().IsElectricalFailed());
+    }
+}
