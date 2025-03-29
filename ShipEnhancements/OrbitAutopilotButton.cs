1d1eb1cc41446996082a2f51649e675cac0e1ed9
Etherpod <etherpod@gmail.com>
2025-03-29 13:11:47 -0700

    Auto align when orbiting

--- ShipEnhancements/OrbitAutopilotButton.cs
+++ ShipEnhancements/OrbitAutopilotButton.cs
@@ -13,6 +13,8 @@ public class OrbitAutopilotButton : CockpitButtonSwitch
 
         _orbitAutopilot.OnAbortAutopilot += OnAbortAutopilot;
         _orbitAutopilot.OnInitOrbit += OnInitOrbit;
+        GlobalMessenger<OWRigidbody>.AddListener("EnterFlightConsole", OnEnterFlightConsole);
+        GlobalMessenger.AddListener("ExitFlightConsole", OnExitFlightConsole);
     }
 
     public override void OnChangeActiveEvent()
@@ -50,10 +52,23 @@ public class OrbitAutopilotButton : CockpitButtonSwitch
         }
     }
 
+    private void OnEnterFlightConsole(OWRigidbody shipBody)
+    {
+        AlignShipWithReferenceFrame align = _orbitAutopilot.GetComponent<AlignShipWithReferenceFrame>();
+        align.enabled = false;
+    }
+
+    private void OnExitFlightConsole()
+    {
+        _orbitAutopilot.GetComponent<AlignShipWithReferenceFrame>().enabled = IsActivated();
+    }
+
     protected override void OnDestroy()
     {
         base.OnDestroy();
         _orbitAutopilot.OnAbortAutopilot -= OnAbortAutopilot;
         _orbitAutopilot.OnInitOrbit -= OnInitOrbit;
+        GlobalMessenger<OWRigidbody>.RemoveListener("EnterFlightConsole", OnEnterFlightConsole);
+        GlobalMessenger.RemoveListener("ExitFlightConsole", OnExitFlightConsole);
     }
 }
