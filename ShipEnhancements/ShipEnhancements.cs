fc22db532e4469022169a5422aecd1c532f44718
Etherpod <etherpod@gmail.com>
2025-04-17 22:19:45 -0700

    disable debug messages

--- ShipEnhancements/ShipEnhancements.cs
+++ ShipEnhancements/ShipEnhancements.cs
@@ -2503,6 +2503,8 @@ public class ShipEnhancements : ModBehaviour
 
     public static void WriteDebugMessage(object msg, bool warning = false, bool error = false)
     {
+        return;
+
         msg ??= "null";
 
         if (warning)
