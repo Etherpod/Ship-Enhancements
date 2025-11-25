8880421c0abb3cb26a50835c986f51ef06c02367
ditzy <ditzy.zy@pm.me>
2025-11-25 07:54:43 -0600

    switch to bespoke settings code generation

---
+++ ShipEnhancements.Settings.Generator/SettingData.cs
@@ -0,0 +1,9 @@
+﻿namespace ShipEnhancements.Settings.Generator;
+
+internal class SettingData
+{
+    public string Name { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string FieldType { get; set; } = string.Empty;
+    public string DefaultValue { get; set; } = string.Empty;
+};
---
+++ ShipEnhancements.Settings.Generator/SettingsGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using DitzyExtensions;
+using DitzyExtensions.Collection;
+using static ShipEnhancements.Settings.Generator.GenUtils;
+
+namespace ShipEnhancements.Settings.Generator;
+
+public static class SettingsGenerator
+{
+    private static readonly string GenTargetPath = "gen";
+    private static readonly string SettingTargetFile = $"{GenTargetPath}/SESetting.g.cs";
+    private static readonly string SettingsTargetFile = $"{GenTargetPath}/SESettings.g.cs";
+    private static readonly string PresetTargetFile = $"{GenTargetPath}/SEPreset.g.cs";
+    private static readonly IList<string> KeysToExclude = ["preset"];
+
+    private static void Main(string[] args)
+    {
+        Directory.CreateDirectory(GenTargetPath);
+        
+        var settingData = ParseConfig();
+        CreateSourceFiles(settingData);
+        
+        Console.WriteLine("Done!");
+    }
+
+    private static IList<SettingData> ParseConfig()
+    {
+        Console.WriteLine("Parsing config for settings...");
+
+        var jsonText = File.ReadAllText("default-config.json");
+        var config = JsonNode.Parse(jsonText)!;
+
+        var settingData = config["settings"]!.AsObject()
+            .Where(entry => !KeysToExclude.Contains(entry.Key.AsLower()))
+            .Where(entry => entry.Value!["type"]!.ToString().AsLower() != "separator")
+            .Select(entry =>
+            {
+                var settingData = new SettingData {
+                    Name = entry.Key,
+                    Title = entry.Value!["title"]!.ToString(),
+                };
+                
+                var defaultValue = entry.Value!["value"]!.AsValue();
+                switch (defaultValue.GetValueKind())
+                {
+                    case JsonValueKind.Number:
+                        settingData.FieldType = "float";
+                        settingData.DefaultValue = $"{defaultValue.Deserialize<float>()}f";
+                        break;
+                    case JsonValueKind.String:
+                        settingData.FieldType = "string";
+                        settingData.DefaultValue = S(defaultValue.Deserialize<string>()!);
+                        break;
+                    case JsonValueKind.True:
+                    case JsonValueKind.False:
+                        settingData.FieldType = "bool";
+                        settingData.DefaultValue = $"{defaultValue.Deserialize<bool>()}".AsLower();
+                        break;
+                }
+                
+                return settingData;
+            })
+            .AsList();
+
+        return settingData;
+    }
+
+    private static void CreateSourceFiles(IList<SettingData> data)
+    {
+        Console.WriteLine("Creating SESetting file...");
+        File.WriteAllText(SettingTargetFile, SESettingSource);
+        Console.WriteLine("Creating SESettings file...");
+        File.WriteAllText(SettingsTargetFile, CreateSettingsSource(data));
+        Console.WriteLine("Creating SEPreset file...");
+        File.WriteAllText(PresetTargetFile, CreatePresetSource(data));
+    }
+}
